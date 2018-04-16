a31bac419046d17a762476b38e621755a781d7ef
Ehsan <mohammadi.ehsan1994@gmail.com>
2018-04-16 20:03:16 +0430

    First commit

---
+++ Assets/Scripts/Ball_Controller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball_Controller : MonoBehaviour {
+    
+    public float speed = 0.1f;
+    public float jump = 1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        // Up or Down
+        if (Input.GetKey(KeyCode.UpArrow))
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, speed));
+        else if (Input.GetKey(KeyCode.DownArrow))
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -speed));
+
+        // Left or Right
+        if (Input.GetKey(KeyCode.LeftArrow))
+            GetComponent<Rigidbody>().AddForce(new Vector3(-speed, 0, 0));
+        else if (Input.GetKey(KeyCode.RightArrow))
+            GetComponent<Rigidbody>().AddForce(new Vector3(speed, 0, 0));
+
+        // Jump
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, jump, 0));
+	}
+}
---
+++ Assets/Scripts/Camera_Follow.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Follow : MonoBehaviour {
+
+    public Transform target;
+    private Vector3 distance;
+
+	// Use this for initialization
+	void Start () 
+    {
+        distance = target.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        transform.position = target.position - distance;
+	}
+}
