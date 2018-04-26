5c766105e9411fc82fe11b9323ce60b73fa722b5
Ehsan <mohammadi.ehsan1994@gmail.com>
2018-04-26 08:25:07 +0430

    Assets updated

---
+++ Assets/Scripts/Move_Station.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Station : MonoBehaviour {
+
+    public GameObject nextStation;
+    public bool move = true;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && move)
+        {
+            other.transform.position = nextStation.transform.position;
+            move = false;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            move = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Station.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Station : MonoBehaviour {
+
+    public GameObject nextBall;
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            GameObject newBall = Instantiate(nextBall, other.transform.position, Quaternion.identity);
+            GameObject prevBall = Camera.main.transform.parent.GetComponent<Camera_Follow>().target.gameObject;
+            Camera.main.transform.parent.GetComponent<Camera_Follow>().target = newBall.transform;
+            Destroy(prevBall);
+            
+        }
+    }
+}
